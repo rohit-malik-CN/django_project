bc7787f2e89731ea9264bcaba628f4325d47a0ee
rohit-malik <2016csb1055@iitrpr.ac.in>
2019-05-26 17:41:17 +0530

    bug fixing

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp1',
     'swagger_ui',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,8 +55,18 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080'
+]
+CORS_ORIGIN_REGEX_WHITELIST = [
+    'http://localhost:8080'
+]
 ROOT_URLCONF = 'django_project.urls'
 
 TEMPLATES = [
