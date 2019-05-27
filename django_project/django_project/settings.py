1d6c29ff8e0d862c878e04683d041fb8be5d59c3
rohit-malik <2016csb1055@iitrpr.ac.in>
2019-05-27 11:36:20 +0530

    commit

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -54,6 +54,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080'
+]
+CORS_ORIGIN_REGEX_WHITELIST = [
+    'http://localhost:8080'
 ]
 
 ROOT_URLCONF = 'django_project.urls'
